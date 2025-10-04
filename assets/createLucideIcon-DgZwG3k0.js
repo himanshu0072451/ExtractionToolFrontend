47b4144cd10a1709b531dcd4812ecff678b4b972
Himanshu <himanshuingle42@gmail.com>
2025-10-04 23:59:38 +0530

    Updates

--- assets/createLucideIcon-DgZwG3k0.js
+++ assets/createLucideIcon-DgZwG3k0.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-C7z9ssSZ.js";/**
+import{r as s}from"./index-en6SSg0z.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
