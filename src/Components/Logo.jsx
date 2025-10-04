f980a225c65d64aaacd8c32f119101bdeb14410c
Himanshu Ingle <134830474+himanshu0072451@users.noreply.github.com>
2025-10-04 23:48:41 +0530

    Update Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,17 +1,37 @@
+// import React from "react";
+
+// const Logo = ({ color }) => {
+//   return (
+//     <div className="top h-[40px]">
+//       <div className="logo h-full w-full">
+//         <img
+//           className={`${color === "white" ? "invert" : ""}`}
+//           src={`${import.meta.env.BASE_URL}logo1.svg`}
+//           alt="LOGO!"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Logo;
+
+
+
+
 import React from "react";
 
 const Logo = ({ color }) => {
   return (
-    <div className="top h-[40px]">
-      <div className="logo h-full w-full">
-        <img
-          className={`${color === "white" ? "invert" : ""}`}
-          src={`${import.meta.env.BASE_URL}logo1.svg`}
-          alt="LOGO!"
-        />
-      </div>
+    <div className="top h-[40px] w-[150px]">
+      <img
+        src={`${import.meta.env.BASE_URL}logo1.svg`}
+        alt="LOGO!"
+        className={`${color === "white" ? "invert" : ""} h-full w-full object-contain`}
+      />
     </div>
   );
 };
 
 export default Logo;
+
