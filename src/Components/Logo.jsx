176bbfc5b072565b8953d5f1ef56c40372e9240f
Himanshu Ingle <134830474+himanshu0072451@users.noreply.github.com>
2025-10-04 23:36:55 +0530

    Update Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "@/assets/logo1.svg"; // path relative to src
 
 const Logo = ({ color }) => {
   return (
     <div className="top h-[40px]">
       <div className="logo h-full w-full">
-        <img className={`${color === "white" ? "invert" : null}`} src="/logo1.svg" alt="LOGO!" />
+        <img className={`${color === "white" ? "invert" : ""}`} src={logo} alt="LOGO!" />
       </div>
     </div>
   );
