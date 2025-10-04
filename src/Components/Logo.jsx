12a4e21fd37eca0b920838092ea05570942f0faf
Himanshu <himanshuingle42@gmail.com>
2025-10-04 23:56:58 +0530

    update: logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,33 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// const Logo = ({ color }) => {
-//   return (
-//     <div className="top h-[40px]">
-//       <div className="logo h-full w-full">
-//         <img
-//           className={`${color === "white" ? "invert" : ""}`}
-//           src={`${import.meta.env.BASE_URL}logo1.svg`}
-//           alt="LOGO!"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Logo;
-
-
-
-const Logo = () => {
+const Logo = ({ color }) => {
   return (
-    <img
-      src="https://himanshu0072451.github.io/ExtractionToolFrontend/logo1.svg"
-      alt="LOG!"
-      style={{ width: "150px", height: "40px" }}
-    />
+    <div className="top h-[40px]">
+      <div className="logo h-full w-full">
+        <img
+          className={`${color === "white" ? "invert" : ""}`}
+          src={`${import.meta.env.BASE_URL}logo1.svg`}
+          alt="logo!"
+        />
+      </div>
+    </div>
   );
 };
 
 export default Logo;
-
-
